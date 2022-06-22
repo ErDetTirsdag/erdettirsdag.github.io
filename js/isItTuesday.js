f8d644da9e28d881415fbfb7b3cb80984ec3192b
Brede Fritjof Klausen <Brede-klausen@hotmail.com>
2022-06-22 14:46:49 +0200

    Add option for custom question

--- js/isItTuesday.js
+++ js/isItTuesday.js
@@ -1,6 +1,19 @@
 function isItTuesday() {
+    if (window.location.search !== "" && window.location.search.includes("title")) {
+        const urlParams = new URLSearchParams(window.location.search);
+        changeTitle(urlParams.get("title"))
+    } else
+        changeAnswer(new Date().getDay() === 2)
+}
+
+function changeTitle(titleContent) {
+    document.getElementById("title").innerText = titleContent
+    changeAnswer(Math.floor(Math.random() * 2))
+}
+
+function changeAnswer(isTrue) {
     const elem = document.getElementById("answer");
-    if (new Date().getDay() === 2) {
+    if (isTrue) {
         elem.innerHTML = "Ja";
         elem.className = "yes";
     } else {
